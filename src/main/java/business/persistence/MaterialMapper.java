def6f0a910707bfe698ecff014819103b5c7c2ea
DanielD99 <71074562+DanielD99@users.noreply.github.com>
2021-12-06 21:42:09 +0100

    MaterialFacade.java & MaterialMapper.java

--- src/main/java/business/persistence/MaterialMapper.java
+++ src/main/java/business/persistence/MaterialMapper.java
@@ -8,48 +8,41 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MaterialMapper {
 
-        private Database database;
+    private Database database;
 
-        public MaterialMapper(Database database)
-        {
-            this.database = database;
-        }
+    public MaterialMapper(Database database) {
+        this.database = database;
+    }
 
-        public Material importAllMaterials(String material_id) throws UserException
-        {
-            try (Connection connection = database.connect())
-            {
-                String sql = "SELECT materiale_id, length FROM materiale WHERE price=? AND unit=?";
-
-                try (PreparedStatement ps = connection.prepareStatement(sql))
-                {
-                    ps.setString(1, price);
-                    ps.setString(2, unit);
-                    ResultSet rs = ps.executeQuery();
-                    if (rs.next())
-                    {
-                        String role = rs.getString("unit");
-                        Double price = rs.getDouble("price");
-          //              User user = new User(email, password, role);
-                        Material material = new Material(344,"stk");
-                        material.setId(id);
-                        return material;
-                    } else
-                    {
-                        throw new UserException("Could not validate user");
+    public List<Material> getAllMaterials(int id) throws UserException {
+        List<Material> materialList = null;
+        try (Connection connection = database.connect()) {
+            String sql = "SELECT * from material where material_id=?";
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setInt(1, id);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    if (materialList == null) {
+                        materialList = new ArrayList<>();
                     }
+
+                    String material_id = rs.getString("material_id");
+                    String short_description = rs.getString("short_description");
+                    Material material = new Material(material_id, short_description);
+                    materialList.add(material);
+
                 }
-                catch (SQLException ex)
-                {
-                    throw new UserException(ex.getMessage());
-                }
+            } catch (SQLException ex) {
+                throw new UserException(ex.getMessage());
             }
-            catch (SQLException ex)
-            {
-                throw new UserException("Connection to database could not be established");
-            }
+        } catch (SQLException ex) {
+            throw new UserException(ex.getMessage());
         }
+        return materialList;
+    }
 }
---
+++ src/main/java/business/services/MaterialFacade.java
@@ -0,0 +1,26 @@
+package business.services;
+
+import business.entities.Material;
+import business.entities.User;
+import business.exceptions.UserException;
+import business.persistence.Database;
+import business.persistence.MaterialMapper;
+import business.persistence.UserMapper;
+
+import java.util.List;
+
+public class MaterialFacade {
+    MaterialMapper materialMapper;
+
+    public MaterialFacade(Database database)
+    {
+        materialMapper = new MaterialMapper(database);
+    }
+    public List<Material> getAllMaterials;
+
+    public List <Material> getAllMaterials() throws UserException
+    {
+        return getAllMaterials();
+    }
+
+}
