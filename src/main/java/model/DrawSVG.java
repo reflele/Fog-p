e47036402dfb61a87594737676e875355ad1c0ec
DanielD99 <71074562+DanielD99@users.noreply.github.com>
2021-12-23 17:49:14 +0100

    DrawSVG.java metode, der tegner carport ud fra request id samt forbindelse til database og mere dynamik

---
+++ src/main/java/model/DrawSVG.java
@@ -0,0 +1,103 @@
+package model;
+
+import business.entities.SVG;
+import business.exceptions.UserException;
+import business.persistence.Database;
+import business.services.MaterialFacade;
+import business.services.RequestFacade;
+import model.Calculate;
+
+import java.util.List;
+
+public class DrawSVG {
+
+    RequestFacade requestFacade;
+
+    public DrawSVG(Database database) {
+        requestFacade = new RequestFacade(database);
+    }
+
+
+    public SVG draw(int reqId) throws UserException {
+
+
+        List<Request> requestList = requestFacade.getAllRequests();
+        Calculate calculate;
+        int raftersDistance = 50;
+        int shortSide;
+        int longSide;
+        int carportLength;
+        int carportWidth;
+        int a = 0;
+        while (true) {
+            if (requestList.get(a).getId() == reqId) {
+                carportLength = requestList.get(a).getLength();
+                carportWidth = requestList.get(a).getWidth();
+                break;
+            }
+            a++;
+        }
+
+        if (carportLength > carportWidth) {
+            longSide = carportLength;
+            shortSide = carportWidth;
+        } else {
+            longSide = carportWidth;
+            shortSide = carportLength;
+        }
+
+
+        double rafterWidth = 4.5;
+        double raftersCount = longSide / raftersDistance;
+
+        int rafterX = 0;
+        int rafterY = 0;
+
+        int postX = raftersDistance;
+
+        int postX1 = 0;
+        int postY1 = 0;
+
+        int beamX = 0;
+        int upperBeamPostY = (int) (shortSide * 0.12);
+        int lowerBeamPostY = (int) (shortSide * 0.88);
+
+        double beamWidth = 4.5;
+        double postWidth = 6;
+        double postHeight = 6;
+        double postCount = (longSide / raftersDistance);
+
+        //viewbox
+        SVG svg = new SVG(0, 0, "0 0 800 600", 100, 50);
+
+        //carport omrids
+        svg.addRect(0, 0, shortSide, longSide);
+
+        //rafter
+        for (int i = 0; i < raftersCount; i++) {
+            svg.addRect(rafterX, rafterY, shortSide, rafterWidth);
+            rafterX = rafterX + raftersDistance;
+        }
+
+        //beams
+        svg.addRect(beamX, upperBeamPostY, beamWidth, longSide);
+        svg.addRect(beamX, lowerBeamPostY, beamWidth, longSide);
+
+        //posts row 1
+        for (int i = 0; i < postCount; i++) {
+            svg.addRect(postX, upperBeamPostY, postHeight, postWidth);
+            postX = postX + (raftersDistance * 3);
+        }
+
+        postX = raftersDistance;
+        //posts row 2
+        for (int i = 0; i < postCount; i++) {
+            svg.addRect(postX, lowerBeamPostY, postHeight, postWidth);
+            postX = postX + (raftersDistance * 3);
+        }
+        return svg;
+
+    }
+
+
+}
--- src/main/java/web/commands/ShowSvgCommand.java
+++ src/main/java/web/commands/ShowSvgCommand.java
@@ -2,7 +2,9 @@ package web.commands;
 
 import business.exceptions.UserException;
 import business.entities.SVG;
+import business.persistence.Database;
 import model.Calculate;
+import model.DrawSVG;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,46 +20,13 @@ public class ShowSvgCommand extends CommandUnprotectedPage {
     //her laves svg tegning
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException {
-        Calculate calculate = new Calculate(database);
 
-        int raftersDistance = (int) calculate.getRaftersDistance();
-        int postDistance = (int) calculate.getPostsDistance();
-        int carportHeight = 600;
-        int carportWidth = 780;
+        DrawSVG drawSVG = new DrawSVG(database);
+
+        SVG carport =  drawSVG.draw(60);
 
 
-        double rafterWidth = 4.5;
-        int raftersCount = 16;
-        int rafterX = 0;
-        int rafterY = 0;
-        int beamX = 0;
-        int upperBeamPostY = (int) (carportHeight * 0.12);
-        int lowerBeamPostY = (int) (carportHeight * 0.88);
-        double beamWidth = 4.5;
-
-        //viewbox
-        SVG svg = new SVG(0, 0, "0 0 800 600", 100, 50);
-
-        //carport omrids
-        svg.addRect(0, 0, carportHeight, carportWidth);
-
-        //rafter
-        for (int i = 0; i < raftersCount; i++) {
-            svg.addRect(rafterX , 0, carportHeight, rafterWidth);
-            rafterX = rafterX + raftersDistance;
-        }
-
-        //beams
-        svg.addRect(beamX, upperBeamPostY, beamWidth, carportWidth);
-        svg.addRect(beamX, lowerBeamPostY, beamWidth, carportWidth);
-
-//        //posts
-//        for (int a = 0; a < 14; a++) {
-//            svg.addRect(100 + postDistance * i, (int) y, 4.5, 4.5);
-//        }
-
-
-        request.setAttribute("svgdrawing", svg.toString());
+        request.setAttribute("svgdrawing", carport.toString());
         return pageToShow;
 
     }
